58af4be318b1d3b61469a5b05976bd4cf69c4750
ColorsYu <yuyu0906yuyu0906@gmail.com>
2026-01-15 14:46:55 +0900

    csvfix

--- Assets/Scripts/MenuSaveCsvButton.cs
+++ Assets/Scripts/MenuSaveCsvButton.cs
@@ -1,3 +1,5 @@
+using System;
+using System.IO;
 using UnityEngine;
 
 public class MenuSaveCsvButton : MonoBehaviour
@@ -6,6 +8,38 @@ public class MenuSaveCsvButton : MonoBehaviour
 
     public void SaveCsv()
     {
-        ConditionSequence.WriteCsv(csvFileName);
+        string uniqueFileName = BuildUniqueFileName(csvFileName);
+        ConditionSequence.WriteCsv(uniqueFileName);
+    }
+
+    static string BuildUniqueFileName(string fileName)
+    {
+        string directory = Path.GetDirectoryName(fileName);
+        string baseName = Path.GetFileNameWithoutExtension(fileName);
+        string extension = Path.GetExtension(fileName);
+        if (string.IsNullOrEmpty(extension))
+        {
+            extension = ".csv";
+        }
+
+        string stamp = DateTime.Now.ToString("yyyyMMdd_HHmmss_fff");
+        string candidateName = string.Format("{0}_{1}{2}", baseName, stamp, extension);
+        string combinedName = string.IsNullOrEmpty(directory)
+            ? candidateName
+            : Path.Combine(directory, candidateName);
+        string path = Path.Combine(Application.persistentDataPath, combinedName);
+
+        int index = 1;
+        while (File.Exists(path))
+        {
+            candidateName = string.Format("{0}_{1}_{2}{3}", baseName, stamp, index, extension);
+            combinedName = string.IsNullOrEmpty(directory)
+                ? candidateName
+                : Path.Combine(directory, candidateName);
+            path = Path.Combine(Application.persistentDataPath, combinedName);
+            index++;
+        }
+
+        return combinedName;
     }
 }
